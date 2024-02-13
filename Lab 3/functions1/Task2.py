51288cdf338c06cc9508c9c153f3a84d44dd9070
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 15:11:49 +0600

    Task2.py

---
+++ Lab 3/functions1/Task2.py
@@ -0,0 +1,8 @@
+# Task 2:
+
+def FahrenheitToC():
+    F = input("Enter the temperature in Fahrenheit: ")
+    C = (5 / 9) * (float(F) - 32)
+    print("The temperature in Centigrade is: " + str(C))
+
+FahrenheitToC()
