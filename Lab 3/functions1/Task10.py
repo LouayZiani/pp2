1c89578cd0ec8623eb362b5b9dc1e63241592993
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 15:50:35 +0600

    Task10.py

--- Lab 3/functions1/Task10.py
+++ Lab 3/functions1/Task10.py
@@ -1,13 +1,16 @@
 # Task 10:
 
-def unique(nums):
-    uniqueList = []
-    for element in nums:
-        if element not in uniqueList:
-            uniqueList.append(element)
-    return uniqueList
+# Write a Python function that takes a list and returns a new list with unique elements of the first list. Note: don't use collection set.
 
-a=input()
-list=[int(nums) for nums in a.split()]
-answer = unique(list)
-print(answer)
+def unique(my_list):
+    unique_list = []
+    for element in my_list:
+        if element not in unique_list:
+            unique_list.append(element)
+    return unique_list
+
+x = input("Enter a list of integers: ")
+lista = [int(num) for num in x.split()]
+
+result = unique(lista)
+print("Unique elements:", result)
