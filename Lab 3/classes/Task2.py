62431c3af1ceea90a967081409613ab0e06ebb19
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 13:12:03 +0600

    Task2.py

--- Lab 3/classes/Task2.py
+++ Lab 3/classes/Task2.py
@@ -1,17 +1,23 @@
 # Task 2: 
 
-class Shape:
+# Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. 
+# Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+
+class Shape():
     def __init__(self):
         pass
-    def area(self):
+    def __area__(self):
         return 0
+
+
+
 class Square(Shape):
     def __init__(self, length):
-        super().__init__()
-        self.length=length
-    def area(self):
-        return self.length*self.length
+        self.length = length
+    def __area__(self):
+        return self.length * self.length
     
-if __name__=="__main__":
-    square=Square(int(input()))
-    print(square.area())
+if __name__== "__main__":
+    square = Square(int(input("Enter the length of one side of your square: ")))
+    print("The Area is:", square.__area__())
+    
