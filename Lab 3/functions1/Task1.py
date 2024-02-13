1c4735b20846c1edd09a25c78a39d13a5ec90f41
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 15:12:02 +0600

    Task1.py

--- Lab 3/functions1/Task1.py
+++ Lab 3/functions1/Task1.py
@@ -6,16 +6,3 @@ def ConvertGtoO():
  ounces = str(ounces)
  print("The value in ounces is: " + ounces)
 
-ConvertGtoO()
-
-# Task 2:
-
-def FahrenheitToC():
-    F = input("Enter the temperature in Fahrenheit: ")
-    C = (5 / 9) * (float(F) - 32)
-    print("The temperature in Centigrade is: " + str(C))
-
-FahrenheitToC()
-
-# Task 3:
-
