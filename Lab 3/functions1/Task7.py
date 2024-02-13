270af3ee77c4fc08cc8b22a05a7ad7e226e346e2
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 15:30:28 +0600

    Task7.py

--- Lab 3/functions1/Task7.py
+++ Lab 3/functions1/Task7.py
@@ -1,12 +1,13 @@
 # Task 7:
 
-def contains33(nums):
-    for i in range(len(nums)-1):
-        if nums[i]==3 and nums[i+1]==3:
+def has_33(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] == 3 and nums[i + 1] == 3:
             return True
     return False
-    
-a=input()
-List=[int(nums) for nums in a.split()]
-result=contains33(List)
-print (result)
+
+numbers = input("Enter a list of ints: ")
+nums = [int(x) for x in numbers.split()]
+
+result = has_33(nums)
+print("Result:", result)
