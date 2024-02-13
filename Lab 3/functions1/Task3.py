68568699c823164d82a3b5fd246c616f4f8e9124
LouayZiani <156789538+LouayZiani@users.noreply.github.com>
2024-02-13 15:14:18 +0600

    Task3.py

--- Lab 3/functions1/Task3.py
+++ Lab 3/functions1/Task3.py
@@ -1,15 +1,15 @@
 # Task 3:
 
-def solve(numheads, numlegs):
-    for chicken in range(numheads+1):
-        rabbit = numheads-chicken
-        if 2*chicken + 4*rabbit == numlegs:
+def solve(number_heads, number_legs):
+    for chicken in range(number_heads+1):
+        rabbit = number_heads-chicken
+        if 2*chicken + 4*rabbit == number_legs:
             return chicken, rabbit
     
 
-answer = solve(numheads=35, numlegs=94)
+answer = solve(number_heads=35, number_legs=94)
 if answer:
     chicken, rabbit = answer
-    print("chickens: ",chicken)
-    print("rabbits: ",rabbit)
+    print("There are",chicken, "chickens")
+    print("There are",rabbit, "rabbits")
   
